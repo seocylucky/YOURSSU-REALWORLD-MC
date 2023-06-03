007a71316ac76cc948c77283cd61403cffb71f90
seocylucky <seocylucky@github.com>
2023-06-03 23:00:30 +0900

    design : realworld template 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 
+import Footer from './Layout/Footer'
+import Header from './Layout/Header'
 import Home from './pages/Home'
 import Register from './pages/Register'
 
 const App = () => {
   return (
     <RecoilRoot>
+      <Header />
       <Routes>
         <Route
           path="/"
@@ -19,6 +22,7 @@ const App = () => {
           element={<Register />}
         />
       </Routes>
+      <Footer />
     </RecoilRoot>
   )
 }
