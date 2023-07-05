10f3f965d64d85330f1d445330df00c387831382
seocylucky <seocylucky@github.com>
2023-07-05 21:29:59 +0900

    feat: home 페이지 구현 작업

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,149 +1,134 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
+import { Cookies } from 'react-cookie'
+import { useRecoilState } from 'recoil'
+
+import ArticleItem from '@/components/ArticleItem'
+
+import { UserState } from '../State/userState'
+import { GetArticleResponse } from '../Types/articles'
 
 const Home = () => {
+  const [currentUser] = useRecoilState(UserState)
+  const [mode, setMode] = useState('Global Feed')
+  const [tags, setTags] = useState<string[]>([])
+  const [articles, setArticles] = useState<GetArticleResponse[]>([])
+  const cookie = new Cookies()
+
+  useEffect(() => {
+    axios.get('https://api.realworld.io/api/tags', {}).then((res) => {
+      console.log(res)
+      setTags(res.data.tags)
+    })
+  }, [])
+
+  useEffect(() => {
+    handleArticle()
+  }, [mode])
+
+  const handleArticle = () => {
+    console.log('handleArticle')
+    if (mode === 'Your Feed') {
+      axios
+        .get('https://api.realworld.io/api/articles/feed?limit=10', {
+          headers: {
+            Authorization: `Bearer ${cookie.get('token')}`,
+          },
+        })
+        .then((res) => {
+          const data = res.data.articles
+          console.log(data)
+          setArticles(data)
+        })
+    } else if (mode === 'Global Feed') {
+      axios.get('https://api.realworld.io/api/articles?limit=10', {}).then((res) => {
+        console.log(res.data.articles)
+        setArticles(res.data.articles)
+        console.log(res.data.articlesCount)
+      })
+    } else {
+      axios.get(`https://api.realworld.io/api/articles?tag=${mode}`, {}).then((res) => {
+        setArticles(res.data.articles)
+      })
+    }
+  }
+
   return (
     <div className="home-page">
-      <div className="banner">
-        <div className="container">
-          <h1 className="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
+      {!currentUser && (
+        <div className="banner">
+          <div className="container">
+            <h1 className="logo-font">conduit</h1>
+            <p>A place to share your knowledge.</p>
+          </div>
         </div>
-      </div>
+      )}
 
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
             <div className="feed-toggle">
               <ul className="nav nav-pills outline-active">
+                {currentUser && (
+                  <li className="nav-item">
+                    <div
+                      className={`nav-link ${mode === 'Your Feed' && 'active'}`}
+                      onClick={() => {
+                        setMode('Your Feed')
+                      }}
+                    >
+                      Your Feed
+                    </div>
+                  </li>
+                )}
                 <li className="nav-item">
-                  <a
-                    className="nav-link disabled"
-                    href=""
-                  >
-                    Your Feed
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    href=""
+                  <div
+                    className={`nav-link ${mode === 'Global Feed' && 'active'}`}
+                    onClick={() => {
+                      setMode('Global Feed')
+                    }}
                   >
                     Global Feed
-                  </a>
+                  </div>
                 </li>
+                {mode !== 'Your Feed' && mode !== 'Global Feed' && (
+                  <li className="nav-item">
+                    <div
+                      className={`nav-link ${
+                        mode !== 'Your Feed' && mode !== 'Global Feed' && 'active'
+                      }`}
+                      onClick={() => {
+                        setMode('Global Feed')
+                      }}
+                    >
+                      <i className="ion-pound"></i>
+                      &nbsp;{mode}
+                    </div>
+                  </li>
+                )}
               </ul>
-            </div>
-
-            <div className="article-preview">
-              <div className="article-meta">
-                <a href="profile.html">
-                  <img src="http://i.imgur.com/Qr71crq.jpg" />
-                </a>
-                <div className="info">
-                  <a
-                    href=""
-                    className="author"
-                  >
-                    Eric Simons
-                  </a>
-                  <span className="date">January 20th</span>
-                </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 29
-                </button>
-              </div>
-              <a
-                href=""
-                className="preview-link"
-              >
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
-
-            <div className="article-preview">
-              <div className="article-meta">
-                <a href="profile.html">
-                  <img src="http://i.imgur.com/N4VcUeJ.jpg" />
-                </a>
-                <div className="info">
-                  <a
-                    href=""
-                    className="author"
-                  >
-                    Albert Pai
-                  </a>
-                  <span className="date">January 20th</span>
-                </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 32
-                </button>
-              </div>
-              <a
-                href=""
-                className="preview-link"
-              >
-                <h1>The song you won&#39;t ever stop singing. No matter how hard you try.</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
+              <ArticleItem
+                articles={articles}
+                handleArticle={handleArticle}
+              />
             </div>
           </div>
 
           <div className="col-md-3">
             <div className="sidebar">
               <p>Popular Tags</p>
-
               <div className="tag-list">
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  programming
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  javascript
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  emberjs
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  angularjs
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  react
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  mean
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  node
-                </a>
-                <a
-                  href=""
-                  className="tag-pill tag-default"
-                >
-                  rails
-                </a>
+                {tags &&
+                  tags.map((tag, index) => (
+                    <div
+                      className="tag-pill tag-default"
+                      key={index}
+                      onClick={() => setMode(`${tag}`)}
+                    >
+                      {tag}
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
